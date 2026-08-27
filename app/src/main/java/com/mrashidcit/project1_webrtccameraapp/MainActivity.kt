556d74e0851c37d0b36865b3f2828753c87ac14b
Rashid Saleem <mrashidcit@gmail.com>
2026-08-27 16:26:27 +0500

    Initial Commit

---
+++ app/src/main/java/com/mrashidcit/project1_webrtccameraapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.mrashidcit.project1_webrtccameraapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mrashidcit.project1_webrtccameraapp.ui.theme.Project1WebRTCCameraAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Project1WebRTCCameraAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Project1WebRTCCameraAppTheme {
+        Greeting("Android")
+    }
+}
